81a73f9eb8c827b8ec588127359e51591b457397
henlee <125648188+henlee1@users.noreply.github.com>
2024-09-17 23:54:48 +0800

    Update server.py

--- server.py
+++ server.py
@@ -32,10 +32,13 @@ from datetime import datetime
 from googleapiclient import discovery
 
 toxic_words = [
+    "black",
     "toes",
     "fingers",
     "police",
     "knee",
+    "knees",
+    "kneee",
     "socks",
     "caprons",
     "capron",
